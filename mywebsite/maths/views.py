771656c3495dd94cb63f18b7114c4da01114afa6
unknown <raja.kavi@gmail.com>
2020-08-18 14:02:54 +0200

    Included Messages

--- mywebsite/maths/views.py
+++ mywebsite/maths/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django import forms
+from django.contrib import messages
+
+
 from django.contrib.auth.forms import UserCreationForm
 from .forms import AdditionForm,RegisterForm
 from .validate import calcscore,validate
 from django.contrib.auth  import login,logout,authenticate 
-
 
 # Create your views here.
 
@@ -54,13 +56,13 @@ def register(request):
         if form.is_valid():
            form.save()
            username=form.cleaned_data.get("username")
+           messages.success(request,f'New User {username} created successfully . Welcome {username}')
            print(username)
-      #     login(request, username)
-           return redirect("/")
+       #    login(request, username)
+#           return redirect("/")
         else:
             for msg in form.error_messages:
-                print(form.error_messages[msg])
-
+                messages.error(request,f' {msg}: {form.error_messages[msg]}')
             return render(request,"maths/register.html",{"form":form})
     form=RegisterForm()
    
