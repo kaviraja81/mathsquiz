75e201b1eea96159259a66f16a18c6e727d67d1a
unknown <raja.kavi@gmail.com>
2020-08-27 12:17:16 +0200

    Added all urls and divided by grades

--- mywebsite/maths/urls.py
+++ mywebsite/maths/urls.py
@@ -2,9 +2,12 @@ from django.urls import path,include
 from . import views
 urlpatterns=[
     path('',views.index,name='index'),
-    path("grade1/add/<int:category>/",views.add,name="add3"),
     path("register/",views.register,name="register"),
     path('', include("django.contrib.auth.urls")),
-    path('grade1/',views.grade1,name="grade1"),
-    path("grade1/add2digit/<int:category>/",views.add,name="add3"),
+    path('grade1/',views.grade,name="grade"),
+    path('grade2/',views.grade,name="grade"),
+    path("grade1/add/<int:category>/",views.add,name="add"),
+    path("grade1/sub/<int:category>/",views.add,name="add"),
+    path("grade2/add/<int:category>/",views.add,name="add"),
+    path("grade2/sub/<int:category>/",views.add,name="add")
 ]
--- mywebsite/maths/views.py
+++ mywebsite/maths/views.py
@@ -21,47 +21,78 @@ def index(request):
 
 # val=[response.POST.get('inputans{}'.format(i)) for i in range(1,11)]
 
-def grade1(response):
+def grade(response):
     return render(response,"maths/grade.html",{})
 
 
 def add(response,category):
     global result
     global num1, num2
+    operator = '+'
+    print(response.get_full_path())
+    # print(response.path)
+    # print(response.build_absolute_uri())
+    valsplit=response.path.split('/')
+    if valsplit[2] == 'add' :
+       operator = '+'
+    elif valsplit[2] == 'sub':
+        operator = '-'
     
+
+    print (operator)
     if response.method == "POST":
         output = response.POST.getlist('inputans')
         print(num1, num2)
+        print(output)
         validoutput, IsValid = validate(output)
         print(validoutput, output, IsValid)
+        
         if not IsValid:
+            print("not valid")
             zippedlist = zip(num1, num2, validoutput, output)
-            return render(response, "maths/addition.html", {"zippedlist": zippedlist})
+            return render(response, "maths/addition.html", {"zippedlist": zippedlist,"operator": operator})
+            
         else:
             output = [int(i) for i in output]
             score = calcscore(output, result)
             username = User.objects.get(username=response.user.username)
 
          #   username=response.user.username
-            t=Score(score=score,grade='1',category='1',username=username)
+            t=Score(score=score,grade=valsplit[1][5],category=valsplit[3],username=username)
             t.save()
             
             return HttpResponse(f'Good Job! Your Score is {score} /10')
     else:
+        ##  Category 1 is Single Digit Addition, 
+        ##  Category 2 is Double Digit Addition
+        #3  Category 3 is Additions with Tens
+        ##  Category 4 is Single Digit Subtraction 
+        ##  Category 5 is Double Digit Subtraction
+        ##  Category 6 is Subtraction wtih tens
         form1 = AdditionForm()
-        if category == 1 :
+        if category == 1 or category == 4 :
             num1 = form1.randnumsingledigit()
             num2 = form1.randnumsingledigit()
-        else : 
+        elif category == 2 or category == 4: 
             num1 = form1.randnumdoubledigit()
             num2 = form1.randnumdoubledigit()
+        elif category == 3 or category == 6 :
+            num1 = form1.randnumsingledigit()
+            num2 = form1.randnumsingledigit()*10 
             
-        output = [0 for i in range(10)]
+        # if operator == '-' :
+        #     [x for x,y in num1 if num1 > num2 else: y  ]
+        #    if num1 > num2 : 
+        #       print (num1 > num2) 
+
+        output = ['' for i in range(10)]
         validval = [True for i in range(10)]
         zippedlist = zip(num1, num2, validval, output)
-        result = num1+num2
+        if operator == '+' :  result = num1+num2 
+        else : result = num1-num2
         validval = [True for i in range(10)]
-        return render(response, "maths/addition.html", {"zippedlist": zippedlist})
+        context={}
+        return render(response, "maths/addition.html", {"zippedlist": zippedlist,"operator": operator})
 
 def register(request):
     
